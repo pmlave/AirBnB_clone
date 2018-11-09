ac454fcae036f650fe1854eecfa28089cc53bdd4
Daniel Ojeda <211@holbertonschool.com>
2018-11-09 19:04:46 +0000

    completed task 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,39 @@ module 'base_model'
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     BaseModel class used with all all other classes
     """
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
+    def __init__(self):
+        '''
+        class constructor for class 'BaseModel'
+        '''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''
         string representation of BaseModel instance
         '''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        '''
+        updates 'updated_at' instance with current datetime
+        '''
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         '''
         dictionary representation of an instance
         '''
         new_dict = dict(self.__dict__)
-        for key, value in self.__dict__.items():
-            if key == 'created_at':
-                new_dict['created_at'] = self.__dict__[key].isoformat()
-            if key == 'updated_at':
-                new_dict['updated_at'] = self.__dict__[key].isoformat()
-        
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
