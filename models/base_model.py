0c3fdbd6f4dbb3c2bb0b546a4982f6efa2b3948c
Paul Lave <pmlave@gmail.com>
2018-11-09 20:47:26 +0000

    Updated base_model and BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,22 @@ class BaseModel:
     BaseModel class used with all all other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         class constructor for class 'BaseModel'
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''
