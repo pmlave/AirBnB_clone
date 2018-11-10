a9b80db0164edea2a4d36ddb6763e3265296ec42
Daniel Ojeda <211@holbertonschool.com>
2018-11-10 01:50:17 +0000

    initial filestorage push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,16 +3,13 @@
 
 from models.base_model import BaseModel
 import json
+from datetime import datetime
 
 class FileStorage:
     """Represents a class FileStorage"""
 
     __file_path = "file.json"
     __objects = {}
-
-    def __init__(self):
-        """Class Instantiation"""
-        pass
 
     def all(self):
         """Returns a dictionary"""
@@ -24,14 +21,20 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
-        type(self).__objects[type(obj).__name__ + '.'+ obj.id]
-        with open(FileStorage.__file_path, "w") as write_file:
-            json_string = json.dump(type(self).__objects, write_file)
+        new_dict = {}
+        for key, value in type(self).__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "a+", encoding='utf-8') as write_file:
+            json.dump(new_dict, write_file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, "r") as read_file:
-                data = json.load(read_file)
+            with open(FileStorage.__file_path, "r", encoding='utf-8') as read_file:
+                type(self).__objects = json.load(read_file)
+            for key, value in type(self).__objects.items():
+                obj = eval(type(self).__objects[key]['__class__'])(**value)
+                type(self).__objects[key] = obj
+                
         except FileNotFoundError:
             pass
