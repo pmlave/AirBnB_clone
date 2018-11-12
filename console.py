b283448baecbf0cc2c18be235c91a84d2a255b7f
Paul Lave <pmlave@gmail.com>
2018-11-12 20:42:19 +0000

    Updated console with fixes to unique ID

--- console.py
+++ console.py
@@ -2,62 +2,83 @@
 """Method Command Interpreter"""
 
 import inspect
-import cmd, sys
+import cmd
+import sys
 import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
+
 class HBNBCommand(cmd.Cmd):
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
+    class_dict = {"BaseModel": BaseModel}
 
     def do_create(self, args):
-        """Creates a new instance of BaseModel, saves it, and prints the id
+        """
+        Creates a new instance of BaseModel, saves it, and prints the id
         Usage: create <class name>
         """
         if not args:
             print("** class name missing **")
-        class_dict = {"BaseModel": BaseModel()}
-        for key, value in class_dict.items():
-            if args == key:
-                new_creation = value
-                models.storage.save()
-                print(new_creation.id)
-            else:
-                print("** class doesn't exist **")
+        else:
+            for key, value in HBNBCommand.class_dict.items():
+                if args == key:
+                    new_creation = value()
+                    models.storage.save()
+                    print(new_creation.id)
+                else:
+                    print("** class doesn't exist **")
 
     def do_show(self, args):
         """Prints the string representation of a specific instance
         Usage: show <class name> <id>
         """
         strings = args.split()
-        if len(strings) == 1:
-            print("** instance id missing **")
-        elif len(strings) == 0:
+        if len(strings) == 0:
             print("** class name missing **")
-        elif strings[0] != 'BaseModel':
+        elif strings[0] not in HBNBCommand.class_dict.keys():
             print("** class doesn't exist **")
+        elif len(strings) == 1:
+            print("** instance id missing **")
         else:
             key_value = strings[0] + '.' + strings[1]
             try:
                 print(models.storage.all()[key_value])
             except KeyError:
                 print("** no instance found **")
+
     def do_destroy(self, args):
         """Deletes an instance
         Usage: destroy <class name> <id>
         """
+        strings = args.split()
+        if len(strings) == 0:
+            print("** class name missing **")
+        elif strings[0] not in HBNBCommand.class_dict.keys():
+            print("** class doesn't exist **")
+        elif len(strings) == 1:
+            print("** instance id missing **")
+        else:
+            key_value = strings[0] + '.' + strings[1]
+            try:
+                del models.storage.all()[key_value]
+                models.storage.save()
+            except KeyError:
+                print("** no instance found **")
 
     def do_all(self, args):
         """Prints a string representation of all instances, can include class
         name to specify only instances of that class
         Usage: destroy <class name> <id>
         """
+        print(models.storage.all())
 
     def do_update(self, args):
         """Update an instance.
         Usage: update <class name> <id> <attribute name> "<attribute value>"
         """
+
     def do_quit(self, args):
         """Quits the program
         """
@@ -80,6 +101,7 @@ class HBNBCommand(cmd.Cmd):
         """Doesn't execute anything when user enter an empty line
         """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
