df5cab17a2dfdc0aa969aaca8d3321736858c9e4
Daniel Ojeda <211@holbertonschool.com>
2018-11-13 20:11:09 +0000

    changes to test files

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,17 +8,4 @@ class amenityTest(unittest.TestCase):
     '''
     Test cases for base_model class
     '''
-    def setUp(self):
-        '''
-        simple set up
-        '''
-        pass
-
-    def test_id_is_string(self):
-        '''
-        testing to verify id is a string
-        '''
-        self.assertIsInstance(id, str)
-
-if __name__ == '__main__':
-    unittest.main()
+    pass
